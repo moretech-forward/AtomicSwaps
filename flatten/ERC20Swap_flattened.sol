94c67c21061e3601e1d53b146185aaf53beb9e0b
Sergey <mysteryon88@yandex.ru>
2024-05-04 15:20:41 +0300

    update erc20 flatten

--- flatten/ERC20Swap_flattened.sol
+++ flatten/ERC20Swap_flattened.sol
@@ -36,6 +36,9 @@ abstract contract AtomicSwap is Owned {
     /// @notice Auxiliary variable for frontend
     address public immutable myAddr;
 
+    /// @notice The secret key used to unlock the swap.
+    string public key;
+
     /// @notice One day in timestamp
     /// @dev Used as a time unit for defining deadlines, specifically to protect side B in transactions.
     uint256 constant DAY = 86400;
@@ -47,10 +50,6 @@ abstract contract AtomicSwap is Owned {
     /// @notice Deadline after which the swap cannot be accepted.
     /// @dev Represented as a Unix timestamp, this is used to enforce the time limitation on the swap.
     uint256 public deadline;
-
-    /// @notice Emitted when the swap is confirmed successfully with the correct key.
-    /// @param key The secret key used to unlock the swap.
-    event SwapConfirmed(string indexed key);
 
     constructor() {
         myAddr = address(this);
@@ -154,7 +153,7 @@ contract AtomicERC20Swap is AtomicSwap {
         require(keccak256(abi.encodePacked(_key)) == hashKey, "Invalid key");
         require(block.timestamp <= deadline, "Deadline has passed");
         // Publishing a key
-        emit SwapConfirmed(_key);
+        key = _key;
         // Balance transfer of ERC20 token to the caller (otherParty)
         uint256 balance = token.balanceOf(address(this));
         require(token.transfer(msg.sender, balance), "Transfer failed");
